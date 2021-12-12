7381e344e8c55a5e5b941630422263f80b04d789
Bharath14 <bharathsaiellanti@gmail.com>
2021-12-12 15:23:03 +0530

    Fourth Commit with all testing done.

--- src/game/ChessConsole.java
+++ src/game/ChessConsole.java
@@ -22,58 +22,76 @@ public class ChessConsole {
         while(game.getGameStatus() == GameStatus.CONTINUE)
         {
             int flagwhite = 0;
-            while(flagwhite == 0) {
-                //System.out.println("White turn");
-                System.out.println("Enter Source");
-                int x1 = sc.nextInt();
-                int y1 = sc.nextInt();
-                Cell whitesource = cells[x1][y1];
-                white.setSource(whitesource);
-
-                System.out.println("Enter Destination");
-                int x2 = sc.nextInt();
-                int y2 = sc.nextInt();
-                Cell whitedest = cells[x2][y2];
-                white.setDest(whitedest);
-                boolean result = white.move(game);
-                if(result == true)
-                {
-                    flagwhite = 1;
-                }
-                else
-                {
-                    flagwhite = 0;
-                }
-                //System.out.println("Original Color:"+game.getCurrentTurn().toCharacter());
-            }
-            GameInfo.printState(game);
-            if(game.getGameStatus() == GameStatus.CONTINUE)
+            System.out.println("Do you want to resign ? (Y/N)");
+            String wresign = sc.next();
+            if(wresign.equals("Y"))
             {
-                int flagblack = 0;
-                while(flagblack == 0) {
-                    //System.out.println("Black turn");
+                game.setGameStatus(BLACKWINS);
+                break;
+            }
+            else {
+                while (flagwhite == 0) {
+                    //System.out.println("White turn");
                     System.out.println("Enter Source");
-                    int x3 = sc.nextInt();
-                    int y3 = sc.nextInt();
-                    Cell blacksource = cells[x3][y3];
-                    black.setSource(blacksource, game);
-
-                    System.out.println("Enter Destination");
-                    int x4 = sc.nextInt();
-                    int y4 = sc.nextInt();
-                    Cell blackdest = cells[x4][y4];
-                    black.setDest(blackdest);
-                    if(black.move(game)==true)
-                    {
-                        flagblack = 1;
+                    int x1 = sc.nextInt();
+                    int y1 = sc.nextInt();
+                    boolean result = false;
+                    if (x1 >= 0 && x1 < 8 && y1 >= 0 && y1 < 8) {
+                        Cell whitesource = cells[x1][y1];
+                        if (white.setSource(whitesource, game)) {
+                            System.out.println("Enter Destination");
+                            int x2 = sc.nextInt();
+                            int y2 = sc.nextInt();
+                            if (x2 >= 0 && x2 < 8 && y2 >= 0 && y2 < 8) {
+                                Cell whitedest = cells[x2][y2];
+                                white.setDest(whitedest);
+                                result = white.move(game);
+                            } else {
+                                System.out.println("Invalid board coordinates! Please try again.");
+                            }
+                        }
+                    } else {
+                        System.out.println("Invalid board coordinates! Please try again.");
                     }
-                    else
-                    {
-                        flagblack = 0;
+                    if (result == true) {
+                        flagwhite = 1;
+                    } else {
+                        flagwhite = 0;
                     }
+                    //System.out.println("Original Color:"+game.getCurrentTurn().toCharacter());
                 }
                 GameInfo.printState(game);
-                //System.out.println("Original color:"+game.getCurrentTurn().toCharacter());
+            }
+            if(game.getGameStatus() == GameStatus.CONTINUE) {
+                System.out.println("Do you want to resign ? (Y/N)");
+                String bresign = sc.next();
+                if (bresign.equals("Y")) {
+                    game.setGameStatus(WHITEWINS);
+                    break;
+                } else {
+                    int flagblack = 0;
+                    while (flagblack == 0) {
+                        //System.out.println("Black turn");
+                        System.out.println("Enter Source");
+                        int x3 = sc.nextInt();
+                        int y3 = sc.nextInt();
+                        Cell blacksource = cells[x3][y3];
+                        black.setSource(blacksource, game);
+
+                        System.out.println("Enter Destination");
+                        int x4 = sc.nextInt();
+                        int y4 = sc.nextInt();
+                        Cell blackdest = cells[x4][y4];
+                        black.setDest(blackdest);
+                        if (black.move(game) == true) {
+                            flagblack = 1;
+                        } else {
+                            flagblack = 0;
+                        }
+                    }
+                    GameInfo.printState(game);
+                    //System.out.println("Original color:"+game.getCurrentTurn().toCharacter());
+                }
             }
         }
 
